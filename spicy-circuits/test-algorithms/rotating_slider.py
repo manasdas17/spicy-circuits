0ab31f789f2c171d085b873f1b381f8d20ffaa7b
ryannorton <nort.ryan@gmail.com>
2014-03-23 05:57:30 -0500

    increased speed of scaling window with numba and refactorization

--- spicy-circuits/test-algorithms/rotating_slider.py
+++ spicy-circuits/test-algorithms/rotating_slider.py
@@ -2,43 +2,44 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 import time
+from numba import jit
 
-# checks whether or not window is on line
 def displayImage(src):
 	cv2.imshow('image',src)
 	cv2.waitKey(0)
 	cv2.destroyAllWindows() 
 
+@jit
 def windowOnLine(window):	
-	wrongColumnsChecked = 0
-	columnHeight = len(window)
-	currentRow = 0
+	wrongColumns = 0
+	numRows = len(window)
+	currentRow = numRows / 2
 	while True:
-		if (wrongColumnsChecked >= columnHeight): 			
+		if (wrongColumns >= numRows): 			
 			return False
 		if (window[currentRow, 0] == 1 
 			and window[currentRow, 1] == 1):
 			return True
 		else:
-			currentRow += 1
-			wrongColumnsChecked += 1
+			currentRow = (currentRow + 1) % numRows
+			wrongColumns += 1
 
-def scanLine(angle=0):
-	img = cv2.imread('/Users/Ryan/Desktop/test3.png',0) # get grayscale image
-	ret,binimg = cv2.threshold(img,100,1,cv2.THRESH_BINARY_INV)
-	winUp, winDown, winLeft, winRight = 620, 660, 440, 441 # starting window location
-	while windowOnLine(binimg[winUp:winDown, winLeft:winLeft+2]):
-		# imgUpdate = img.copy()
-		# cv2.rectangle(imgUpdate, (winLeft, winUp), (winRight, winDown), 120, 2)
-		# displayImage(imgUpdate) 
-		winLeft, winRight = winLeft + 5, winRight + 5 # move window right
+def scanLine(img, winHeight, startPos, angle=0, step=2, dst=None):	
+	winUp, winDown = startPos[0]-(winHeight/2), startPos[0]+(winHeight/2)
+	winLeft, winRight = startPos[1], startPos[1] + 1
+	window = img[winUp:winDown, winLeft:winLeft+2]
+	while windowOnLine(window):
+		if dst != None:
+			imgUpdate = dst.copy()
+			cv2.rectangle(imgUpdate, (winLeft, winUp), (winRight, winDown), 120, 2)
+			displayImage(imgUpdate) 
+		winLeft, winRight = winLeft + step, winRight + step
+		window = img[winUp:winDown, winLeft:winLeft+2]
 	compLeftNode = (np.mean((winUp, winDown)), winLeft)
-	cv2.rectangle(img, (winLeft-14,int(compLeftNode[0])-9), (winLeft+4,int(compLeftNode[0])+9), 100, -1)
-	# displayImage(img)
-
-start = time.clock()
-for x in xrange(100):
-	scanLine()
-print time.clock()-start
+	if dst != None:	
+		imgUpdate = dst.copy()
+		cv2.rectangle(imgUpdate, (winLeft-14,int(compLeftNode[0])-9), (winLeft+4,int(compLeftNode[0])+9), 100, -1)
+		displayImage(imgUpdate)
+	return compLeftNode
 
 
