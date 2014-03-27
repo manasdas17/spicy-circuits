471481707e2b2be6e5e95a9da0bb534bee086729
ryannorton <nort.ryan@gmail.com>
2014-03-27 03:26:48 -0500

    fixed bugs, added comments

--- spicy-circuits/test-algorithms/rotating_slider.py
+++ spicy-circuits/test-algorithms/rotating_slider.py
@@ -97,9 +97,9 @@ def scanLine(img, startPos, angle, winHeight=6, step=2, dst=None):
 			window = img[winLeft:winRight, winBack:winBack+2]
 			winBack, winFront = startPos[1] + stepCoords[0], startPos[1] + stepCoords[0]
 		if 90 <= angle <= 270:
-			compLeftNode = (int(np.mean((winLeft, winRight))), winBack + (step * 4))
+			compLeftNode = (int(np.mean((winLeft, winRight))), winBack + (step * 5))
 		else:
-			compLeftNode = (int(np.mean((winLeft, winRight))), winBack - (step * 4))
+			compLeftNode = (int(np.mean((winLeft, winRight))), winBack - (step * 5))
 		if dst != None:	
 			imgUpdate = dst.copy()
 			cv2.rectangle(imgUpdate, (compLeftNode[1]-step,compLeftNode[0]-9), (compLeftNode[1]+9,compLeftNode[0]+9), 100, 10)
@@ -122,9 +122,9 @@ def scanLine(img, startPos, angle, winHeight=6, step=2, dst=None):
 			window = img[winBack:winBack+2, winLeft:winRight]
 			winBack, winFront = startPos[0] - stepCoords[1], startPos[0] - stepCoords[1]
 		if 0 <= angle <= 180: 
-			compLeftNode = (winBack + (step * 4), int(np.mean((winLeft, winRight))))
+			compLeftNode = (winBack + (step * 5), int(np.mean((winLeft, winRight))))
 		else:
-			compLeftNode = (winBack - (step * 4), int(np.mean((winLeft, winRight))))
+			compLeftNode = (winBack - (step * 5), int(np.mean((winLeft, winRight))))
 		if dst != None:	
 			imgUpdate = dst.copy()
 			cv2.rectangle(imgUpdate, (compLeftNode[1]-9,compLeftNode[0]-9), (compLeftNode[1]+9,compLeftNode[0]+9), 100, 10)
@@ -132,64 +132,92 @@ def scanLine(img, startPos, angle, winHeight=6, step=2, dst=None):
 		return compLeftNode
 
 def distance(p0, p1):
-    return (p0[0] - p1[0])**2 + (p0[1] - p1[1])**2
+    return ((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)**.5
 
-drawImg = cv2.imread('/Users/Ryan/Desktop/test3.png',1)
-img = cv2.imread('/Users/Ryan/Desktop/test3.png',0) # get grayscale image
-ret,binimg = cv2.threshold(img,100,1,cv2.THRESH_BINARY_INV)
-imgHeight, imgWidth = len(binimg), len(binimg[0])
+# generate list of coordinates of black pixels in tuple form.
+@jit
+def getBlackPx(img):
+	blackPxToCheck = []
+	for row in enumerate(img):
+		for px in enumerate(row[1]):
+			if px[1] == 1:
+				blackPxToCheck.append((row[0], px[0]))
+	return blackPxToCheck
 
-def detectNodes():	
+def detectNodes(distThresh=20, histogram=False, periodicUpdate=False):	
+	# generate random starting point on black pixel 
 	while True:
 		randX, randY = random.randint(0,imgWidth - 1), random.randint(0,imgHeight - 1)
 		if binimg[randY, randX] == 1:
 			break
 	startPoint = (randY,randX)	
 	startPoint = (randY,randX)	
+	# list of points to check, starting with starting point. 
+	# nodes found appended to list. Nodes checked popped from list
 	toCheck = [startPoint]
+	# if a node is in this list, we already checked it. Don't check again.
 	checked = []
+	# coordinates of nodes found
 	found = []
+	# done when no more nodes to check
 	while toCheck:
 		origin = toCheck[-1]
-		checked.append(origin)
-		nodes = []
-		for x in range(0,370):	
-			nodes.append(scanLine(binimg, origin, x, winHeight = 10))
-		windowDist = [distance(x,origin) for x in nodes]
-		peaks = peakdetect(windowDist, lookahead = 6, delta = 100)[0]
+		checked.append(toCheck.pop())
+		# list of possible node coordinates, indexed by angle
+		possibleNodes = []
+		# run scanline algorithm for all angles in circle
+		for x in range(0,370):
+			possibleNodes.append(scanLine(binimg, origin, x, winHeight = 8))
+		# list of distances from where window fell off line to origin
+		windowDist = [distance(node,origin) for node in possibleNodes]
+		# nodes we're interested in are where the window travelled farthest
+		peaks = peakdetect(windowDist, lookahead = 5, delta = 10)[0]
 		if len(peaks) == 0:
 			break
-		peakX, peakY = zip(*peaks)
-		peakX = [e for e in peakX]
-		toCheck.pop()
-		for e in peakX:
-			node = nodes[e]
-			tooClose = False
-			for j in checked:
-				if distance(j, node) < 600:
+		peakAngles, peakDistances = zip(*peaks)
+		# make sure potential node isn't too close to already found nodes
+		# to avoid clusters of nodes
+		for peakNode in [possibleNodes[angle] for angle in peakAngles]:
+			tooClose = False	
+			for foundNode in found:
+				if distance(foundNode, peakNode) < distThresh:
 					tooClose = True
 					break
-			if node not in checked and tooClose == False:	
-				toCheck.append(node)
-				found.append(node)	
-		# plt.plot(windowDist)
-		# plt.plot(peakX, peakY, 'ro')
-		# plt.show()		
-		imgUpdate = drawImg.copy()		
-		for x in nodes:
-			cv2.rectangle(imgUpdate, (x[1]-3,x[0]-3), (x[1]+3,x[0]+3), (255,0,0), 1)	
-		for node in found:
-			cv2.rectangle(imgUpdate, (node[1]-8,node[0]-8), (node[1]+8,node[0]+8), (50,255,50), -1)
-		for x in nodes:
-			cv2.rectangle(imgUpdate, (origin[1]-7,origin[0]-7), (origin[1]+7,origin[0]+7), (0,0,255), 2)
-		displayImage(imgUpdate)
+			if not tooClose:	
+				toCheck.append(peakNode)
+				found.append(peakNode)	
+		if histogram == True:
+			plt.plot(windowDist)
+			plt.plot(peakAngles, peakDistances, 'ro')
+			plt.show()			
+		if periodicUpdate == True:	
+			imgUpdate = drawImg.copy()				
+			for x in possibleNodes:
+				cv2.rectangle(imgUpdate, (x[1]-3,x[0]-3), (x[1]+3,x[0]+3), (255,0,0), 1)	
+			for node in found:
+				cv2.rectangle(imgUpdate, (node[1]-8,node[0]-8), (node[1]+8,node[0]+8), (50,255,50), -1)
+			for x in possibleNodes:
+				cv2.rectangle(imgUpdate, (origin[1]-7,origin[0]-7), (origin[1]+7,origin[0]+7), (0,0,255), 2)
+			displayImage(imgUpdate)
+	imgUpdate = drawImg.copy()	
 	for node in found:
 		cv2.rectangle(imgUpdate, (node[1]-8,node[0]-8), (node[1]+8,node[0]+8), (50,255,50), -1)
 	displayImage(imgUpdate)
-# start = time.clock()
-# for x in xrange(100):	
-# 	start1 = time.clock()
-# 	detectNodes()
-# 	print x + 1, time.clock() - start1
-# print time.clock() - start
-detectNodes()
+
+drawImg = cv2.imread('/Users/Ryan/Desktop/test3.png',1)
+img = cv2.imread('/Users/Ryan/Desktop/test3.png',0) # get grayscale image
+ret,binimg = cv2.threshold(img,100,1,cv2.THRESH_BINARY_INV)
+#print getBlackPx(binimg)
+imgHeight, imgWidth = len(binimg), len(binimg[0])
+detectNodes(periodicUpdate=True)
+
+
+
+
+
+
+
+
+
+
+
