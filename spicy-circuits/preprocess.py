59b280c5e84ddcec1b6663f0c4d5569cb2a8131e
tabchas <tabchas@gmail.com>
2014-04-13 20:08:46 -0500

    updated preproces code

--- spicy-circuits/preprocess.py
+++ spicy-circuits/preprocess.py
@@ -1,32 +1,16 @@
 
-import matplotlib.pyplot as plt
+import cv2
+import numpy as np
 
-from skimage import data
-from skimage.filter import threshold_otsu, threshold_adaptive
-from scipy.misc import imread
+#Converts image to binary with option to invert
+def convertToBinary(img, invert = 0):
+    img = cv2.medianBlur(img, 5)
 
-image = imread('/Users/Tabish/Desktop/spicy-circuits/pictures/Circuit1/c1-pen.JPG', True)
+    if invert == 0:
+        ret3, th3 = cv2.threshold(img, 255, 0, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    elif invert == 1:
+        ret3, th3 = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    #Resize image
+    img = cv2.resize(th3, (0,0), fx=0.2, fy=0.2)
 
-global_thresh = threshold_otsu(image)
-binary_global = image > global_thresh
-
-block_size = 40
-binary_adaptive = threshold_adaptive(image, block_size, offset=10)
-
-fig, axes = plt.subplots(nrows=3, figsize=(7, 8))
-ax0, ax1, ax2 = axes
-plt.gray()
-
-ax0.imshow(image)
-ax0.set_title('Image')
-
-ax1.imshow(binary_global)
-ax1.set_title('Global thresholding')
-
-ax2.imshow(binary_adaptive)
-ax2.set_title('Adaptive thresholding')
-
-for ax in axes:
-    ax.axis('off')
-
-plt.show()
+    return img
