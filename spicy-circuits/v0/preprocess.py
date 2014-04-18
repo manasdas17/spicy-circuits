9cc4e08605383732a63f2ab93c047859ee4a6d66
ryannorton <nort.ryan@gmail.com>
2014-04-18 16:08:33 -0500

    added preprocess function

--- spicy-circuits/v0/preprocess.py
+++ spicy-circuits/v0/preprocess.py
@@ -1,14 +1,37 @@
-
 import cv2
 import numpy as np
 
-#Converts image to binary with option to invert
-def convertToBinary(img, invert = False):
-    img = cv2.medianBlur(img, 5)
 
-    if invert == False:
-        thresh, imbin = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    elif invert == True:
-        thresh, imbin = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+"""
+Preprocesses an image. Open file -> Resize -> Grayscale -> Thresholding -> Binary
 
-    return imbin
+Inputs: 
+	img: image path
+	size: (width, height)
+	threshInv: option to invert when converting to binary
+
+Outputs: Resized image, Grayscale image, Thresholded image, Binary image
+"""
+def preprocess(img, size, threshInv=False):
+
+	# open file
+	img = cv2.imread(img, 1)
+
+	# resize
+	img = cv2.resize(img, size)	
+	
+	# grayscale
+	imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	
+	# threshold
+	if threshInv == True:
+		ret,imthresh = cv2.threshold(imgray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+	else:
+		ret,imthresh = cv2.threshold(imgray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)		
+
+	# binary
+	imbin = imthresh
+	imbin[imbin == 255] = 1
+
+	return img, imgray, imthresh, imbin
+
